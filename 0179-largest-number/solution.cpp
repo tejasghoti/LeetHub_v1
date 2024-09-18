ad7f05258a83eca7e75bd35f35fa59d1d7dd71ac
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2024-09-18 15:55:24 +0000

    [LeetCode Sync] Runtime - 3 ms (74.86%), Memory - 17.4 MB (5.64%)

---
+++ 0179-largest-number/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        
+        vector<string> strNums;
+        for (int num : nums) {
+            strNums.push_back(to_string(num));
+        }
+        
+        sort(strNums.begin(), strNums.end(), [](const string &a, const string &b) {
+            return a + b > b + a;
+        });
+        string result;
+        for (string &num : strNums) {
+            result += num;
+        }
+        return result[0] == '0' ? "0" : result;
+    }
+};
+
