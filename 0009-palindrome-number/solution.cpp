dc38951d2e55496fa39a55f00c6c5925bdbd8a09
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2025-07-22 18:08:03 +0000

    [LeetCode Sync] Runtime - 2 ms (50.70%), Memory - 8.6 MB (65.75%)

---
+++ 0009-palindrome-number/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+    long long rev = 0; 
+        int num = x;
+        while (x > 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
+        }return num == rev;
+    }
+};
