f0fa433c8cfc83cf8a30a983aea710f1343d7283
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2025-07-27 12:11:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 28.2 MB (37.42%)

--- 0169-majority-element/solution.cpp
+++ 0169-majority-element/solution.cpp
@@ -1,23 +1,25 @@
-/*class Solution {
+class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> freq;
+        unordered_map<int, int> cnt;
         int n = nums.size();
         for(int num : nums){
-            freq[num]++;
-            if(freq[num] > n/2){
+            cnt[num]++;
+            if(cnt[num] > n/2){
                 return num;
             }
         }
         return -1;
     }
 };
-*/class Solution {
+
+/*class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         return nums[nums.size() / 2];
     }
 };
+*/
 
 
