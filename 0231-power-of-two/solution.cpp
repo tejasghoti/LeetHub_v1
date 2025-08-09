16b5fa6a30d5a2e8905e9b068c4811beaf33f565
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2025-08-09 19:40:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (82.52%)

--- 0231-power-of-two/solution.cpp
+++ 0231-power-of-two/solution.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0) {
-            return false;
+        if(n <=0)return false;
+        while(n%2 == 0){
+            n/=2;
         }
-        while (n % 2 == 0) {
-            n /= 2;
-        }
-        return n == true;
+        return n ==true;
     }
 };
+
 
