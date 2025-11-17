0b6d5f09c2e3a574dec7b954a89e1321afeffd3c
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2025-11-17 17:26:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.9 MB (78.11%)

--- 0125-valid-palindrome/solution.cpp
+++ 0125-valid-palindrome/solution.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        regex re("[^a-zA-Z0-9]");
-        s = regex_replace(s, re, "");
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
-        string reversed = s;
-        reverse(reversed.begin(), reversed.end());
-        return s == reversed;
+        int n=s.length();
+        int i=0;
+        int j=n-1;
+
+        while(i<j){
+            //ignore if not alnum
+            if(!isalnum(s[i])){
+                i++;
+            }
+            else if(!isalnum(s[j])){
+                j--;
+            }
+            else if(tolower(s[i]) != tolower(s[j])){
+                return false;
+            }
+            else{
+                i++;
+                j--;
+            }
+            
+        }
+        return true;
     }
 };
