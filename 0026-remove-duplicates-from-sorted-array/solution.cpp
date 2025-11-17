6cc86a8f526bb20f33ef571bfd7ece2489a59f6a
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2025-11-17 17:30:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 22.5 MB (98.31%)

--- 0026-remove-duplicates-from-sorted-array/solution.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
     int index = 1;
-    for(int i = 1;i<nums.size(); i++){
+    int n = nums.size();
+    for(int i = 1;i< n; i++){
         if( nums[i]!= nums[i-1] ){
          
             nums[index] = nums [i];
                 index++;
+                
            
         }
 
