3909e658f799e4058917ff71c86e63220c341f29
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2025-11-19 05:04:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.9 MB (12.86%)

--- 0027-remove-element/solution.cpp
+++ 0027-remove-element/solution.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    int removeElement(vector<int>& a, int v) {
+    int removeElement(vector<int>& a, int val) {
         int i = 0;
-        for (int j = 0; j < a.size(); j++) {
-            if (a[j] != v) {
+        int n= a.size();
+        for (int j = 0; j < n; j++) {
+            if (a[j] != val) {
                 a[i] = a[j];
                 i++;
             }
