30658702d7d31edfc0a68f84e9d49c040da23a6c
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2025-11-19 04:29:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 22.7 MB (19.30%)

--- 0026-remove-duplicates-from-sorted-array/solution.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-    int index = 1;
-    int n = nums.size();
-    for(int i = 1;i< n; i++){
-        if( nums[i]!= nums[i-1] ){
-         
-            nums[index] = nums [i];
-                index++;
-                
-           
+        int i = 0;
+        int n=nums.size();
+        //2pointer approach
+        for(int j=0; j<n; j++){
+            //if jth element is not equal to ith means its unique
+            if(nums[j] != nums[i]){
+                i++;
+                //so, i++ and nums ith position vr nums[j] as both will be unique
+                nums[i] = nums[j];
+            }
         }
-
+        //return total uniq. elements
+        return i+1;
     }
-         return index;
-
-    }
-    
 };
