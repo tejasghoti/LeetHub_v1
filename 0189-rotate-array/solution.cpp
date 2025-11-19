0de3095e8668c02ced2e74f2f1d51774e5dfe14e
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2025-11-19 07:30:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 29.6 MB (70.31%)

--- 0189-rotate-array/solution.cpp
+++ 0189-rotate-array/solution.cpp
@@ -1,14 +1,11 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;
+        k = k%n;//to handle k > n
+        //shevtun ele. rotate karun pudhe aanar
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin() + k);
         reverse(nums.begin() + k, nums.end());
     }
 };
-
