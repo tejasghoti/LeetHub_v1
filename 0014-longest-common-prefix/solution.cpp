813a74966dad64d6345bdeba1a97c8e822daf4a1
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-06-25 14:26:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.8 MB (57.87%)

--- 0014-longest-common-prefix/solution.cpp
+++ 0014-longest-common-prefix/solution.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        //string ans= 0;
-        if(strs.empty()) {
+        int n = strs.size();
+        if(strs.empty()){
             return "";
         }
-        //for(int i; i < string.length(); i++){
 
-        //}
-        for (int i = 0; i < strs[0].length(); i++) {
+        for (int i=0; i<strs[0].length(); i++){
             char ans = strs[0][i];
-
-            for (int j = 1; j < strs.size(); j++) {
-                if (i == strs[j].length() || strs[j][i] != ans) {
-                    return strs[0].substr(0, i);
+            for(int j=1; j<n; j++){
+                if(i == strs[j].length() || strs[j][i] != ans){
+                    return strs[0].substr(0,i);
                 }
+                //j ran out of character or template(ans) does'nt matches
             }
         }
-        
         return strs[0];
     }
-
-    
 };
