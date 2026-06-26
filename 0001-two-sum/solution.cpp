6ee366dc3c7f96a63677f76752ec25f180ba0c4f
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-06-26 06:34:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.8 MB (39.08%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,15 +1,49 @@
+/* APPROACH 1- brute force 
 class Solution {
- public:
-  vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> numToIndex;
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                if(nums[i] + nums[j] == target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
 
-    for (int i = 0; i < nums.size(); ++i) {
-      if (const auto it = numToIndex.find(target - nums[i]);
-          it != numToIndex.cend())
-        return {it->second, i};
-      numToIndex[nums[i]] = i;
     }
+};
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
 
-    throw;
-  }
+        vector<pair<int, int>> arr;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            arr.push_back({nums[i], i});
+        }
+
+        sort(arr.begin(), arr.end());
+
+        int l = 0;
+        int r = n - 1;
+
+        while (l < r) {
+
+            int sum = arr[l].first + arr[r].first;
+            if (sum == target) {
+                return {arr[l].second, arr[r].second};
+            }
+            else if (sum < target) {
+                l++;
+            }
+            else {
+                r--;
+            }
+        }
+
+        return {};
+    }
 };
