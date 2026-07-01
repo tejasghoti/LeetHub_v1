192534564e66d9c007dd734a01389667a1daf3ac
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-07-01 14:12:53 +0000

    [LeetCode Sync] Runtime - 307 ms (79.18%), Memory - 0.0B (100.00%)

--- 0178-rank-scores/solution.sql
+++ 0178-rank-scores/solution.sql
@@ -1,6 +1,3 @@
--- Window function version (if allowed by judge)
-SELECT
-  score,
-  DENSE_RANK() OVER (ORDER BY score DESC) AS `rank`
-FROM Scores
-ORDER BY score DESC;
+SELECT score, 
+    DENSE_RANK() OVER(ORDER BY score DESC) AS 'rank' 
+FROM Scores;
