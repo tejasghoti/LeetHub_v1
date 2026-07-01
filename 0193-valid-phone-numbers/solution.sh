98c983231dfefa9fd4292b5bba03c4ae8949e633
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-07-01 14:16:36 +0000

    [LeetCode Sync] Runtime - 70 ms (21.37%), Memory - 3.4 MB (68.52%)

--- 0193-valid-phone-numbers/solution.sh
+++ 0193-valid-phone-numbers/solution.sh
@@ -1,3 +1,1 @@
-# Read from the file file.txt and output all valid phone numbers to stdout.
-grep -E '^(\([0-9]{3}\) [0-9]{3}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4})$' file.txt
-
+grep -e "^[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}$" -e "^([0-9]\{3\}) [0-9]\{3\}\-[0-9]\{4\}$" file.txt
