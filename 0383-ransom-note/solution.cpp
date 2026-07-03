bf2b5c46bbca8c0c759af6e2474a64da6ce5480e
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-07-03 13:49:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.8 MB (49.67%)

---
+++ 0383-ransom-note/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if (ransomNote.size() > magazine.size()) {
+            return false;
+        }
+
+        vector<int> freq(26, 0);
+
+        for (char c : magazine) {
+            freq[c - 'a']++;
+        }
+
+        for (char c : ransomNote) {
+            if (freq[c - 'a'] == 0) {
+                return false;
+            }
+            freq[c - 'a']--;
+        }
+
+        return true;
+    }
+};
