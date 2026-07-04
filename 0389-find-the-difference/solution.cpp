20b13edb176250d9ed083c02b6181bfb9a56c929
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-07-04 12:19:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.2 MB (78.82%)

---
+++ 0389-find-the-difference/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        long long sum = 0;
+        long long diff = 0;
+        for (char c : t) {
+            sum += c - 'a';
+        }
+        for (char c : s) {
+            diff += c - 'a';
+        }
+        return char(sum - diff + 'a');
+    }
+};
