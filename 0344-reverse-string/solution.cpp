b35830e3c72864e021e76cb158ddf648a93cdcb9
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-07-05 14:57:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 27.3 MB (15.96%)

--- 0344-reverse-string/solution.cpp
+++ 0344-reverse-string/solution.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(),s.end());
-    }
+        int start = 0;
+        int end = s.size() - 1 ;
+        while(start<end){
+            char temp = s[start];
+            s[start]=s[end];
+            s[end]=temp;
+            start++;
+            end--;
+        }
+        }
+    
 };
