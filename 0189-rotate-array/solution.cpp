93d3f4a8d5420116f71ce97b2055f84a33a87b99
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-07-05 12:55:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 29.5 MB (94.75%)

--- 0189-rotate-array/solution.cpp
+++ 0189-rotate-array/solution.cpp
@@ -4,8 +4,16 @@ public:
         int n = nums.size();
         k = k%n;//to handle k > n
         //shevtun ele. rotate karun pudhe aanar
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+        Reverse(nums,0, n-1);
+        Reverse(nums,0, k-1);
+        Reverse(nums,k, n-1);
+    }
+
+    void Reverse(vector<int>& nums, int l, int r){
+        while(l<r){
+            swap(nums[l],nums[r]);
+            l++;
+            r--;
+        }
     }
 };
