da65b6453ad7c7baabc77312d65c433907e4b600
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-07-07 11:51:02 +0000

    [LeetCode Sync] Runtime - 362 ms (86.05%), Memory - 0.0B (100.00%)

---
+++ 0601-human-traffic-of-stadium/solution.sql
@@ -0,0 +1,21 @@
+WITH cte AS
+(
+    SELECT *,
+           id - ROW_NUMBER() OVER(ORDER BY id) AS grp
+    FROM Stadium
+    WHERE people >= 100
+)
+
+SELECT
+    id,
+    visit_date,
+    people
+FROM cte
+WHERE grp IN
+(
+    SELECT grp
+    FROM cte
+    GROUP BY grp
+    HAVING COUNT(*) >= 3
+)
+ORDER BY visit_date;
