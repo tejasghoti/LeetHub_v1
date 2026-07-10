214be86a03632255db0823ac8d45ec2ebd336e61
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-07-10 12:59:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.9 MB (39.08%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -15,6 +15,8 @@ public:
     }
 };
 */
+
+/*APPROACH 2 - sorting and summing
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -42,6 +44,28 @@ public:
             else {
                 r--;
             }
+        }
+
+        return {};
+    }
+};
+*/
+//most optimal apprach hashmaps
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int,int> mp;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            int complement = target - nums[i];
+
+            if(mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
         }
 
         return {};
