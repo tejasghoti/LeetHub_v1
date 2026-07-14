42bbd5d287582832e24a703464ba6a0963c7d921
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-07-14 17:39:02 +0000

    [LeetCode Sync] Runtime - 608 ms (23.18%), Memory - 0.0B (100.00%)

--- 1908-recyclable-and-low-fat-products/solution.sql
+++ 1908-recyclable-and-low-fat-products/solution.sql
@@ -1,7 +1,3 @@
 # Write your MySQL query statement below
-select product_id from products
-where low_fats = 'Y' AND recyclable = 'Y'
-order by 1.asc
-
-
-
+select product_id from Products 
+where low_fats ='Y' and recyclable = 'Y'
