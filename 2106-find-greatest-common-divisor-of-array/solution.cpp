cdd4f53b038209c400cce961e30d4c59fa538c72
Tejas Ghoti <134047592+tejasghoti@users.noreply.github.com>
2026-07-18 10:46:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.3 MB (34.84%)

---
+++ 2106-find-greatest-common-divisor-of-array/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int mini = nums[0];
+        int maxi = nums[n-1];
+        int ans = gcd(mini, maxi);
+        return ans;
+    }
+};
